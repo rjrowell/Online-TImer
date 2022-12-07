4ee4798d2cef97ea04eea1daf3174dfcc1e030e2
rjrowell <106752494+rjrowell@users.noreply.github.com>
2022-12-07 16:19:10 +0000

    Added functionality to the enter time form. Also removed mongoose because it is unnecessary

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
 const express = require('express');
+const { redirect } = require('express/lib/response');
 const res = require('express/lib/response');
+const shortid = require('shortid');
 const app = express();
+let theDate = new Date;
 
 // using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
 // app.use("/route", express.static("foldername"));
-app.use('/public', express.static('public'))
+app.use('/public', express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
 app.set('view engine', 'ejs')
-app.get('/',(req,resp)=>{
-    resp.render('index')
+app.get('/',(req,res)=>{
+    res.render('index');
 })
 
-app.listen(process.env.PORT || 5000);
+app.post('/GetTimer',async(req,res)=>{
+    let hours = await req.body.Hours; 
+    let minutes = await req.body.Minutes;
+    let seconds = await req.body.Seconds;
+    let length = "timeToMilliseconds(hours,minutes,seconds)"//TODO This
+    let start = theDate.getTime()
+    let data = [start,length]
+    res.redirect('/timer'+'?data='+data);
+})
+
+app.get('/:timerUrl',(req,res)=>{
+    var data = req.query.data
+    console.log(data)
+    res.render('timer');
+})
+
+app.listen(process.env.PORT || 5000);//defaults to port 5000
