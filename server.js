9ac3babcc930c85598573ba1754ae203c03e3221
rjrowell <106752494+rjrowell@users.noreply.github.com>
2022-12-15 13:14:32 +0000

    Making the util file, to tidy up the code.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { redirect } = require('express/lib/response');
 const res = require('express/lib/response');
+const utilities = require('./utils')
 const shortid = require('shortid');
 const app = express();
 // using app.use to serve up static CSS files in public/assets/ folder when /public link is called in ejs files
@@ -18,7 +19,7 @@ app.post('/GetTimer',async(req,res)=>{
     let hours = await req.body.Hours; 
     let minutes = await req.body.Minutes;
     let seconds = await req.body.Seconds;
-    let length = timeToMilliseconds(hours,minutes,seconds);
+    let length = utilities.timeToMilliseconds(hours,minutes,seconds);
     let start =  new Date().getTime();
     let data = start + length;
     res.redirect('/timer'+'?data='+data);//passes the data on as a query string
@@ -28,29 +29,12 @@ app.get('/:timerUrl',(req,res)=>{
     var timeLeft;
     var data = req.query.data;
     console.log(data + " " +  new Date().getTime())
-    res.render('timer',{timeLeft:timeLeft});
+    res.render('timer');
     setInterval(function counter(){
         timeLeft = data -  new Date().getTime();//TODO display time left on page 
-        timeLeft = msToTime(timeLeft);
+        timeLeft = utilities.msToTime(timeLeft);
     }, 1000);
     
 })
 
 app.listen(process.env.PORT || 5000);//defaults to port 5000
-
-//utilities below
-function timeToMilliseconds(hours,minutes,seconds){
-    return hours * 3600000 + minutes * 60000 + seconds * 1000;
-}
-
-function msToTime(duration) {
-    var seconds = Math.floor((duration / 1000) % 60),
-      minutes = Math.floor((duration / (1000 * 60)) % 60),
-      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
-  
-    hours = (hours < 10) ? "0" + hours : hours;
-    minutes = (minutes < 10) ? "0" + minutes : minutes;
-    seconds = (seconds < 10) ? "0" + seconds : seconds;
-  
-    return hours + ":" + minutes + ":" + seconds;
-  }
